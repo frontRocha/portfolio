4aaf203d692a1c8385bf352abb4a5f9a8858120c
estudantedehtml <mauriciopedeo007@gmail.com>
2022-12-31 16:25:21 -0300

    ajustes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,25 +4,25 @@ export default function Header() {
     return (
         <header className="fixed w-full bg-[#91AED6] z-30">
             <nav className="flex flex-wrap justify-between items-center py-4 px-4 gap-4">
-                <h3 className="fontPop font-bold text-white logo px-4 py-1 rounded-full ">MEU PORTFÓLIO</h3>
+                <h3 className="fontPop font-bold text-white logo px-4 py-1 rounded-full">MEU PORTFÓLIO</h3>
                 <ul className="flex items-center gap-4 fontPop text-white">
                     <a href="#section1">
-                        <li>
+                        <li className='hover:text-[#C9B4D5] duration-500'>
                             Home
                         </li>
                     </a>
                     <a href="#section2">
-                        <li>
+                        <li className='hover:text-[#C9B4D5] duration-500'>
                             Sobre mim
                         </li>
                     </a>
                     <a href="#section3">
-                        <li>
+                        <li className='hover:text-[#C9B4D5] duration-500'>
                             Projetos
                         </li>
                     </a>
                     <a href="#section4">
-                        <li>
+                        <li className='hover:text-[#C9B4D5] duration-500'>
                             Skills
                         </li>
                     </a>
