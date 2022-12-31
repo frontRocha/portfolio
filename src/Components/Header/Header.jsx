1d65ac0978ecb9e9831002201d19b17646327b9e
estudantedehtml <mauriciopedeo007@gmail.com>
2022-12-31 16:36:02 -0300

    ajustes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,22 +6,22 @@ export default function Header() {
             <nav className="flex flex-wrap justify-between items-center py-4 px-4 gap-4">
                 <h3 className="fontPop font-bold text-white logo px-4 py-1 rounded-full">MEU PORTFÓLIO</h3>
                 <ul className="flex items-center gap-4 fontPop text-white">
-                    <a href="#section1  outline-none">
+                    <a href="#section1" className="outline-none">
                         <li className='hover:text-[#C9B4D5] duration-500'>
                             Home
                         </li>
                     </a>
-                    <a href="#section2  outline-none">
+                    <a href="#section2" className="outline-none">
                         <li className='hover:text-[#C9B4D5] duration-500'>
                             Sobre mim
                         </li>
                     </a>
-                    <a href="#section3  outline-none">
+                    <a href="#section3" className="outline-none">
                         <li className='hover:text-[#C9B4D5] duration-500'>
                             Projetos
                         </li>
                     </a>
-                    <a href="#section4  outline-none">
+                    <a href="#section4" className="outline-none>
                         <li className='hover:text-[#C9B4D5] duration-500'>
                             Skills
                         </li>
