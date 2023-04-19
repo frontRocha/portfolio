5cf508b16afc68ccfcea64b13fb5244774dcf988
estudantedehtml <mauriciopedeo007@gmail.com>
2023-04-19 10:43:30 -0300

    ajustes de Projetos

--- src/Data/Pages.js
+++ src/Data/Pages.js
@@ -1,7 +1,7 @@
 export const Pages = [
     {
         "link": "https://freelweb.web.app",
-        "tittle": "WebFree",
+        "title": "WebFree",
         "description": "O objetivo deste projeto é ajudar pessoas a encontrar seu primeiro emprego. Eu me dediquei a ele com muito empenho, pois acredito que ele não só beneficia os outros, mas também me desenvolve profissionalmente.",
         "tumb": "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/Rectangle%208.png",
         "tec": ["https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/react.png","https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/tailwind%202.png?token=GHSAT0AAAAAABW4UW6V67XXCSZT3UZPXXAMY5PYVEQ", "https://raw.githubusercontent.com/frontRocha/portfolio/master/src/assets/firebase.png"],
@@ -10,7 +10,7 @@ export const Pages = [
     },
     {   
         "link": "https://github.com/frontRocha/sylux",
-        "tittle": "SyLux",
+        "title": "SyLux",
         "description": "Criei esta aplicação com o propósito de ajudar as pessoas a organizarem suas finanças. Trabalhar nesse projeto foi uma oportunidade de desenvolvimento para mim como desenvolvedor, pois tive que enfrentar vários desafios técnicos.",
         "tumb": "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/Rectangle%2011.png",
         "tec": ["https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/react.png","https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/tailwind%202.png?token=GHSAT0AAAAAABW4UW6V67XXCSZT3UZPXXAMY5PYVEQ", "https://raw.githubusercontent.com/frontRocha/portfolio/master/src/assets/firebase.png", "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/ts.png"],
@@ -19,7 +19,7 @@ export const Pages = [
     },
     {
         "link": "https://poketypescript.vercel.app/",
-        "tittle": "Poketypescript",
+        "title": "Poketypescript",
         "description": "Este site foi desenvolvido do zero, usando a API do pokéApi. Apesar de ser uma api extremamente utilizada, ela foi utilizada com o intuito de treinar React com TypeScript.",
         "tumb": "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/Rectangle%2017.png",
         "tec": ["https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/react.png","https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/tailwind%202.png?token=GHSAT0AAAAAABW4UW6V67XXCSZT3UZPXXAMY5PYVEQ", "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/ts.png"],
@@ -28,7 +28,7 @@ export const Pages = [
     },
     {
         "link": "https://omisetemplate.vercel.app/",
-        "tittle": "NFT",
+        "title": "NFT",
         "description": "Esta landing page, foi desenvolvida através de um modelo já pronto qu encontrei no figma, nada melhorque treinar as habilidades com css e html e entender tudo sobre landing page.",
         "tumb": "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/Rectangle%2013.png",
         "tec": ["https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/html.png","https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/tailwind%202.png?token=GHSAT0AAAAAABW4UW6V67XXCSZT3UZPXXAMY5PYVEQ", "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/Unofficial_JavaScript_logo_2%201.png"],
@@ -37,7 +37,7 @@ export const Pages = [
     },
     {
         "link": "https://typequizz.vercel.app/",
-        "tittle": "TypeQuizz",
+        "title": "TypeQuizz",
         "description": "Este site foi inspirado em um site já existente, ele originalmente é feito em ReactJs, mas tive o prazer de refatorá-lo em ReactTs, utilizando conceitos interessantes como: useReducer e contextAPI.",
         "tumb": "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/Rectangle%2019.png",
         "tec": ["https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/react.png","https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/tailwind%202.png?token=GHSAT0AAAAAABW4UW6V67XXCSZT3UZPXXAMY5PYVEQ", "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/ts.png"],
@@ -46,8 +46,8 @@ export const Pages = [
     },
     {
         "link": "https://portfoliodevmy.vercel.app/",
-        "tittle": "Portfólio",
-        "description": "A criação do meu portfólio, Como todo bom profissional, ter um portfólio é essencial para que as pessoas possam saber o meu trabalho e do que sou capaz de fazer",
+        "title": "Portfólio",
+        "description": "A criação do meu portfólio, Como todo bom profissional, ter um portfólio é essencial para que as pessoas possam saber o meu trabalho e do que sou capaz de fazer.",
         "tumb": "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/Rectangle%2012.png",
         "tec": ["https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/react.png","https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/tailwind%202.png?token=GHSAT0AAAAAABW4UW6V67XXCSZT3UZPXXAMY5PYVEQ"],
         "tecNames": ["reactJs", "tailwindCss"],
@@ -55,8 +55,18 @@ export const Pages = [
     },
     {
         "link": "/",
-        "tittle": "BarberADM",
-        "description": "Ainda em construção, fazer está aplicação está me fazendo explorar muito do react com typescript, além de entender mais sobre typescript, estou estudando sobre estruturas de software em seu desenvolvimento.",
+        "title": "TableTop",
+        "description": "Ainda em desenvolvimento, Table Top é uma apliacação com um sistema SaaS. O objetivo da aplicação é dinamizar uma administração de lanchonetes e junto, carrega um layout com um design moderno e sofisticado.",
+        "tumb": "https://raw.githubusercontent.com/frontRocha/portfolio/master/src/assets/gear.png",
+        "tec": ["https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/react.png",
+        "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/ts.png", "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/tailwind%202.png?token=GHSAT0AAAAAABW4UW6V67XXCSZT3UZPXXAMY5PYVEQ"],
+        "tecNames": ["reactJs", "typescript", "tailwindCss", "nextJs(backend)"],
+        "id": 7
+    },
+    {
+        "link": "/",
+        "title": "TableTop",
+        "description": "Ainda em desenvolvimento, Table Top é uma apliacação com um sistema SaaS. O objetivo da aplicação é dinamizar uma administração de lanchonetes e junto, carrega um layout com um design moderno e sofisticado.",
         "tumb": "https://raw.githubusercontent.com/frontRocha/portfolio/master/src/assets/gear.png",
         "tec": ["https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/react.png",
         "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/ts.png", "https://raw.githubusercontent.com/frontRocha/portifolio/master/src/assets/tailwind%202.png?token=GHSAT0AAAAAABW4UW6V67XXCSZT3UZPXXAMY5PYVEQ"],
